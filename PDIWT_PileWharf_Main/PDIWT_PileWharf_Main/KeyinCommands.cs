f44e7d6776f6e8643f4b90ecfb7ece82743bca6a
苏东升 <tjdxsds@outlook.com>
2019-07-08 09:28:14 +0800

    Create the scaffolding of piled wharf module to let us to work on. Yifeng is mainly responsible for setting part to store environment related parameters into dgnfile with EC technology. Cui should take care of architecture design for whole project. Su will make the business logic related work to be clear to every team member.
    !!!Caution should be taken to fork your own branch from this rather than modifying main branch!!!

---
+++ PDIWT_PileWharf_Main/PDIWT_PileWharf_Main/KeyinCommands.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PDIWT_PiledWharf_Main
+{
+    public class KeyinCommands
+    {
+        #region Settings Key-ins
+        public static void Settings(String unparsed)
+        {
+            MessageBox.Show("Test!", "This is a functional test");
+        }
+        #endregion
+    }
+}
---
+++ PDIWT_PileWharf_Main/PDIWT_PileWharf_Main/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Resources;
+
+using BM = Bentley.MstnPlatformNET;
+using BD = Bentley.DgnPlatformNET;
+using BCOM = Bentley.Interop.MicroStationDGN;
+
+
+namespace PDIWT_PiledWharf_Main
+{
+    [BM.AddIn(MdlTaskID = "PDIWT_PiledWharf_Main")]
+    internal sealed class Program : BM.AddIn
+    {
+        public static Program Addin = null;
+
+        private Program(IntPtr mdlDesc) : base(mdlDesc)
+        {
+            Addin = this;
+        }
+
+        protected override int Run(string[] commandLine)
+        {
+            ReloadEvent += PDIWT_PiledWharf_Main_ReloadEvent;
+            UnloadedEvent += PDIWT_PiledWharf_Main_UnloadedEvent;
+            return 0;
+        }
+
+        private void PDIWT_PiledWharf_Main_ReloadEvent(BM.AddIn sender, ReloadEventArgs eventArgs)
+        {
+
+        }
+
+        private void PDIWT_PiledWharf_Main_UnloadedEvent(BM.AddIn sender, UnloadedEventArgs eventArgs)
+        {
+
+        }
+
+        public static BD.DgnFile GetActiveDgnFile() => BM.Session.Instance.GetActiveDgnFile();
+        public static BD.DgnModelRef GetActiveDgnModelRef() => BM.Session.Instance.GetActiveDgnModelRef();
+        public static BD.DgnModel GetActiveDgnModel() => BM.Session.Instance.GetActiveDgnModel();
+
+        public static BCOM.Application COM_App
+        {
+            get
+            {
+                return BM.InteropServices.Utilities.ComApp;
+            }
+        }
+
+    }
+}
